fc55b29cc055708c2b7d94c10b3ce79053270f96
owen_adira <owenobezzy@gmail.com>
2018-11-17 23:32:31 +0300

    inject HttpClientModule dependency into weather service as httpClient

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,8 +1,9 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 }
