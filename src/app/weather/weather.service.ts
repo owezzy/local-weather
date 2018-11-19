4789d51c7267e5583cfb9f12c649c90ab49007d7
owen_adira <owenobezzy@gmail.com>
2018-11-19 15:27:00 +0300

    define getCurrentWeather sevive function

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,53 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { environment } from '../../environments/environment'
+import { ICurrentWeather } from '../intefaces'
 
+interface ICurrentWeatherData {
+  weather: [
+    {
+      description: string
+      icon: string
+    }
+  ]
+  main: {
+    temp: number
+  }
+  sys: {
+    country: string
+  }
+  dt: number
+  name: string
+}
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
+
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+    return this.httpClient
+      .get<ICurrentWeatherData>(
+        `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
+          `q=${city},${country}&appid=${environment.appId} `
+      )
+      .pipe(map(data => this.transformToIcurrentWeather(data)))
+  }
+
+  private transformToIcurrentWeather(data: ICurrentWeatherData): ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: data.dt * 1000,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFarenheit(data.main.temp),
+      description: data.weather[0].description,
+    }
+  }
+
+  private convertKelvinToFarenheit(kelvin: number): number {
+    return (kelvin * 9) / 5 - 459.67
+  }
 }
