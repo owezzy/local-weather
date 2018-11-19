df495516b42eb6ddaa9980da57a366489eec2017
owen_adira <owenobezzy@gmail.com>
2018-11-19 18:32:44 +0300

    add currentWeatherComponent to declaration and define providers for test double

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+import { WeatherService } from './weather/weather.service'
+import { WeatherServiceFake } from './weather/weather.service.fake'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, CurrentWeatherComponent],
+      providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
     }).compileComponents()
   }))
 
@@ -26,8 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to owezzy-weather-app!'
-    )
+    expect(compiled.querySelector('h1').textContent).toContain('owezzy-weather-app')
   })
 })
