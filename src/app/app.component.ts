e0135ae1ba245c07ec8b804dfc98869da076bba2
owen_adira <owenobezzy@gmail.com>
2018-11-21 15:02:29 +0300

    add mat-card to layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   template: `
-    <div style="text-align:center">
+    <div>
       <mat-toolbar color="primary">
         <span>{{ title }}</span>
       </mat-toolbar>
-      <div>Your City's Forecast, right now</div>
-      <h2>Current Weather</h2>
-      <app-current-weather></app-current-weather>
+      <div class="content-margin">
+        <div style="text-align:center">Your City's Forecast, right now</div>
+        <div fx-layout="row">
+          <div fxFlex></div>
+          <div fxFlex="500px">
+            <mat-card>
+              <h2>Current Weather</h2>
+              <app-current-weather></app-current-weather>
+            </mat-card>
+          </div>
+          <div fxFlex></div>
+        </div>
+      </div>
     </div>
   `,
 })
