710161130b70659450d845e70135f9b026d1e011
owen_adira <owenobezzy@gmail.com>
2018-11-21 14:26:07 +0300

    add mat-toolbar with primary color set as color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <div style="text-align:center">
+      <mat-toolbar color="primary">
+        <span>{{ title }}</span>
+      </mat-toolbar>
+      <div>Your City's Forecast, right now</div>
+      <h2>Current Weather</h2>
+      <app-current-weather></app-current-weather>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'owezzy-weather-app'
