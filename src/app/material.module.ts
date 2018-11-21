2aa61719883f0826f8aa7f099165acd181d3a0d0
owen_adira <owenobezzy@gmail.com>
2018-11-21 12:15:51 +0300

    import material basic components for Button,Toolbar, icons into app

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,8 @@
-import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule {}
