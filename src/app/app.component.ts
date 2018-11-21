818e1818802137d70e4b76242bf97247d19ddfe3
owen_adira <owenobezzy@gmail.com>
2018-11-21 17:39:11 +0300

    use vertical margin and define headline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { Component } from '@angular/core'
       </mat-toolbar>
       <div class="content-margin">
         <div fxLayoutAlign="center">
-          <div class="mat-caption">Your City's Forecast, right now</div>
+          <div class="mat-caption vertical-margin">Your City's Forecast, right now</div>
         </div>
         <div fx-layout="row">
           <div fxFlex></div>
           <mat-card fxFlex="500px">
             <mat-card-header>
               <mat-card-title>
-                <div class="mat-title">Current Weather</div>
+                <div class="mat-headline">Current Weather</div>
               </mat-card-title>
             </mat-card-header>
             <mat-card-content>
